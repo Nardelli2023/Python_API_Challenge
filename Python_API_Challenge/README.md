6f7e0e4f1e2b058bea2fc2bf7d031c66c727d623
Nardelli2023 <145862838+Nardelli2023@users.noreply.github.com>
2023-11-27 17:12:05 -0500

    Add files via upload

---
+++ Python_API_Challenge/README.md
@@ -0,0 +1,2 @@
+# Python_API_Challenge
+Excel homework
---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "30d75300e79c5705bc37a9b0ed4877b8"
+
+# Geoapify API Key
+geoapify_key = "860ed6f9e0614c61aa8a2fa77ffa8e0b"
